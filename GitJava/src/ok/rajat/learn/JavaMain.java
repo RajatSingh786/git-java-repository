d0916c4ca22d5f8005ac1010967ecc80eef82b12
Sony <Sony@192.168.88.100>
2017-04-30 22:33:26 +0530

    first change

--- GitJava/src/ok/rajat/learn/JavaMain.java
+++ GitJava/src/ok/rajat/learn/JavaMain.java
@@ -5,6 +5,7 @@ public class JavaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("i m good git");
+       System.out.println("i m also good git");
 	}
 
 }
