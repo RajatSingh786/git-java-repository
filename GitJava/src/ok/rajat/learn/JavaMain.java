7592908c9b23747bbb051a0f6dcd3949b1c4f0a0
Sony <Sony@192.168.88.100>
2017-04-30 23:08:34 +0530

    add local file

--- GitJava/src/ok/rajat/learn/JavaMain.java
+++ GitJava/src/ok/rajat/learn/JavaMain.java
@@ -6,6 +6,7 @@ public class JavaMain {
 		// TODO Auto-generated method stub
        System.out.println("i m good git");
        System.out.println("i m also good git");
+       System.out.println("i m also git local fetch from git hub");
 	}
 
 }
