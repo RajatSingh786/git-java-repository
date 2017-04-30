b3ce474b18a766ba8227558ffadbf753d660685c
Sony <Sony@192.168.88.100>
2017-04-30 21:52:38 +0530

    inital checkin of GitJava

---
+++ GitJava/src/ok/rajat/learn/JavaMain.java
@@ -0,0 +1,10 @@
+package ok.rajat.learn;
+
+public class JavaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("i m good git");
+	}
+
+}
